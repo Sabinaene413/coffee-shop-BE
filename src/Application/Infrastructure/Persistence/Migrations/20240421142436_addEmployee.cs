d0a359704b55d4aa25a544d59aa65ad3d584e912
Sabinaene413 <e.ene.sabina.s@gmail.com>
2024-04-21 18:11:05 +0300

    migration

---
+++ src/Application/Infrastructure/Persistence/Migrations/20240421142436_addEmployee.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCoffeeShop.Application.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "UserCredentials");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "TransactionTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "ShopProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "ShopProductOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "ShopOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Inventories");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ContactInformation",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SalaryBrut",
+                table: "Employees",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SalaryNet",
+                table: "Employees",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Taxes",
+                table: "Employees",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "UserCredentials",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "PasswordHash", "PasswordSalt" },
+                values: new object[] { "NAocw1PtQ28osHXSWI4kjnrZ/VesYg/w9VyU+wFo9ww=", "Iu+OTlBiW+YXQz3jfpAiQg==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "SalaryBrut",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "SalaryNet",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Taxes",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "UserCredentials",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "TransactionTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Transactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "ShopProducts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "ShopProductOrders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "ShopOrders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Inventories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactInformation",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "TransactionTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "TransactionTypes",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "UserCredentials",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "Active", "PasswordHash", "PasswordSalt" },
+                values: new object[] { true, "1DDRHw1vqHO4M1Fs4lxm9nBnDHQQmEB9Bmde2OjvO9s=", "f6+23h5E23+Ly7jRCgR5bg==" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Active",
+                value: true);
+        }
+    }
+}
